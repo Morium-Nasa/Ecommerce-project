b15e3f7058db106d14f62571e185927cf9e5b5a8
Moriumnasa-Mim <76652494+Moriumnasa-Mim@users.noreply.github.com>
2022-01-01 21:35:16 +0600

    Update home_page.dart

--- Admin_app-main/admin_app/lib/screen/home_page.dart
+++ Admin_app-main/admin_app/lib/screen/home_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:admin_app/screen/login_page.dart';
 import 'package:admin_app/widget/brand_colors.dart';
 import 'package:flutter/material.dart';
@@ -23,18 +25,46 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          tooltip: "Cancel and Return to List",
+          onPressed: () {
+            Navigator.pop(context, true);
+          },
+        ),
+        automaticallyImplyLeading: false,
+        title: Text("home page"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.save),
+            tooltip: "Save Tool & Log out to page",
+            onPressed: () {
+              logOut();
+            },
+          )
+        ],
+      ), //AppBar
       body: Container(
         width: double.infinity,
-        color: Colors.red,
-        child: MaterialButton(
-          onPressed: () {
-            logOut();
-          },
-          child: Text(
-            "Log out",
-            style: myStyles14(),
-          ),
+        // color: Colors.red[100],
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            image: DecorationImage(
+                image: AssetImage("images/2.jpg"), fit: BoxFit.cover)),
+
+        child: Column(
+          children: [
+            SizedBox(
+              height: 80,
+            ),
+            Center(
+              child: Text(
+                "Welcome to Home_Page",
+                style: myStyles20(),
+              ),
+            ),
+          ],
         ),
       ),
     );
