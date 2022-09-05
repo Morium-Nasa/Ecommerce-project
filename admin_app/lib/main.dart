998d899f43ea6b98313ce70581276c9ee2aeb411
Mim <76652494+Morium-Nasa@users.noreply.github.com>
2022-09-05 08:15:54 -0700

    Update main.dart

--- admin_app/lib/main.dart
+++ admin_app/lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blueee,
+        primarySwatch: Colors.blue,
       ),
       home: LoginPage(),
     );
